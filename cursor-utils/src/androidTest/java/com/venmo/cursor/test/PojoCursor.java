b596110dbfba922086c29e6d72abd250595efce9
Robert Cheung <robert.cheung@venmo.com>
2014-12-23 08:54:28 -0800

    remove helper method references

--- cursor-utils/src/androidTest/java/com/venmo/cursor/test/PojoCursor.java
+++ cursor-utils/src/androidTest/java/com/venmo/cursor/test/PojoCursor.java
@@ -4,9 +4,6 @@ import android.database.Cursor;
 
 import com.venmo.cursor.IterableCursorWrapper;
 
-/**
- * Created by ronshapiro on 6/16/14.
- */
 public class PojoCursor extends IterableCursorWrapper<Pojo> {
 
     public PojoCursor(Cursor cursor) {
@@ -16,14 +13,14 @@ public class PojoCursor extends IterableCursorWrapper<Pojo> {
     @Override
     public Pojo peek() {
         return new Pojo(
-                getIntegerHelper("some_int", -1),
-                getLongHelper("some_long", -1l),
-                getFloatHelper("some_float", -1f),
-                getDoubleHelper("some_double", -1d),
-                getShortHelper("some_short", (short) -1),
-                getBooleanHelper("some_boolean", false),
-                getBlobHelper("some_byte_array", new byte[]{-1, -1}),
-                getStringHelper("some_str", "-1")
+                getInteger("some_int", -1),
+                getLong("some_long", -1l),
+                getFloat("some_float", -1f),
+                getDouble("some_double", -1d),
+                getShort("some_short", (short) -1),
+                getBoolean("some_boolean", false),
+                getBlob("some_byte_array", new byte[]{-1, -1}),
+                getString("some_str", "-1")
         );
     }
 }
